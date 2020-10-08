d85e8c7ffeb04d9da0ec5dd5221d2ee8992c667e
Pawel Bak <pawelbak300589@gmail.com>
2020-10-08 16:55:45 +0100

    Add abstract custom error class and update all errors to extend it

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,12 +1,17 @@
-import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class DatabaseConnectionError extends Error {
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
   reason = 'Error connecting to database';
 
   constructor() {
-    super();
+    super('Error connecting to DB');
 
     // Only because we are extending a build in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
   }
 }
