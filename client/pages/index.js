a4c7bf5f6d8f5fbec9d7fac1f421e0e69d6b525d
Pawel Bak <pawelbak300589@gmail.com>
2020-10-15 13:49:39 +0100

    Update index page with current user request implementation

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,27 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    // we are on the server
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // we are on the browser
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+
+  return response.data;
+};
+
+export default LandingPage;
