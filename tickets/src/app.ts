488b3a7c00e9aadace4090928859fff47f453b71
Pawel Bak <pawelbak300589@gmail.com>
2020-10-25 10:17:25 +0000

    Add show route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@pb89tickets/common';
 
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -17,6 +18,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', () => {
   throw new NotFoundError();
